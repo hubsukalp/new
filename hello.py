fe99a0188b09cc3e77555a64d3a9a1049e5015ec
hubsukalp <142014936+hubsukalp@users.noreply.github.com>
2023-08-19 20:03:14 +0530

    Update and rename hello.py to codes.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-if(a>b):
-  print("largest number is ",a)
-elif(a<b):
-  print("largest number is ",b)
-else:
-  print("Both numbers are equal")
