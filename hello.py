8d72505d2e93dacb37cc88134b4e44219c30e426
hubsukalp <142014936+hubsukalp@users.noreply.github.com>
2023-08-19 20:08:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-if(a>b):
-  print("largest number is ",a)
-elif(a<b):
-  print("largest number is ",b)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if (num1 > num2) and (num1 > num3):
+    largest = num1
+elif (num2 > num1) and (num2 > num3):
+    largest = num2
 else:
-  print("Both numbers are equal")
+    largest = num3
+print("The largest number is", largest)
