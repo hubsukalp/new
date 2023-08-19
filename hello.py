37263489c95ab85af794613f9c257dc51f1f4f84
hubsukalp <sukalpwarhekar2004@gmail.com>
2023-08-19 15:52:19 +0530

    update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("hello world")
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+if(a>b):
+  print("largest number is ",a)
+elif(a<b):
+  print("largest number is ",b)
+else:
+  print("Both numbers are equal")
