cc17efc686f8dbc214a320e313bc019723df9fd7
hubsukalp <sukalpwarhekar2004@gmail.com>
2023-08-19 19:54:12 +0530

    codes

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 print("hello world")
+
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 if(a>b):
@@ -7,3 +8,15 @@ elif(a<b):
   print("largest number is ",b)
 else:
   print("Both numbers are equal")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if (num1 > num2) and (num1 > num3):
+    largest = num1
+elif (num2 > num1) and (num2 > num3):
+    largest = num2
+else:
+    largest = num3
+
+print("The largest number is", largest)
